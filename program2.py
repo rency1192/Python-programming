1470dcf16807d67497c1ed2c9fe885d41f7e409e
rency1192 <88083974+rency1192@users.noreply.github.com>
2022-07-06 19:37:04 +0530

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,10 @@
+first=20
+second=5
+third=25
+
+if(first > second and first >third):
+    print("first ",first)
+elif(second >first and second >third):
+    print("second ",second)
+else:
+    print("third ",third)
